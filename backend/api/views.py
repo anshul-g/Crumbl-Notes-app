c20548676c6ad1fd283b45e27758c9a61cb7a21a
Anshul Gandhi <anshulgandhi2001@gmail.com>
2022-12-28 17:37:15 +0530

    Created and registered Note model, created serializer for our model

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Note
+from .serialisers import NoteSerializer
 
 # Create your views here.
+@api_view(['GET'])
+def apiOverview(request) :
+  return Response({'data': 'Helloworld'})
+
+@api_view(['GET'])
+def notesList(request):
+  notes = Note.objects.all()
+  serializer = NoteSerializer(notes, many=True)
+  return Response(serializer.data)
+
+@api_view(['POST'])
+def noteCreate(request):
+  serializer = NoteSerializer(request.data)
+  if serializer.is_valid():
+    serializer.save()
+    return Response(serializer.data)
