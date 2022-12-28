563df9cf2e2500de5f9427de5849a648242cdab2
Anshul Gandhi <anshulgandhi2001@gmail.com>
2022-12-28 22:47:22 +0530

    Designed Login page

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,19 +1,71 @@
-import '../styles/Button.css'
+import '../styles/Button.css';
 
-function Login(){
+function Login() {
   const handleSubmit = (e) => {
     e.preventDefault();
-  }
+  };
 
-  return(
-      <form style={{width: '20vw'}}>
-        <div style={{display: 'flex', flexDirection : 'column', gap: '0.5rem'}}>
-        <input style={{padding: '0.5rem 1rem', border:'1px solid white', borderRadius: '4px', background: 'grey', color:'white'}} type="email" placeholder="name@email.com" />
-        <input style={{padding: '0.5rem 1rem', border:'1px solid white', borderRadius: '4px', background: 'grey', color:'white'}} type="password" placeholder="Password.."></input>
-        </div>
-        <button className={"primaryBtn"} type="submit" onClick={handleSubmit}>Submit</button>
-      </form>
-  )
+  return (
+    <form
+      className="center"
+      style={{
+        width: '20vw',
+        border: '1px solid white',
+        padding: '2rem',
+        textAlign: 'center',
+        borderRadius: '4px',
+      }}
+    >
+      <h2 style={{ marginBottom: '0.25rem' }}>Login</h2>
+      <div
+        style={{
+          height: '5px',
+          background: 'rgb(180,64,255)',
+          marginBottom: '2rem',
+        }}
+      ></div>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          marginBottom: '2rem',
+          textAlign: 'left',
+        }}
+      >
+        <label>Email</label>
+        <input
+          style={{
+            backgroundColor: '#202124',
+            fontFamily: 'Poppins',
+            padding: '0.5rem 1rem',
+            marginBottom: '1rem',
+            border: '1px solid white',
+            borderRadius: '4px',
+            color: 'white',
+          }}
+          type="email"
+          placeholder="name@email.com"
+        />
+        <label>Password</label>
+        <input
+          style={{
+            backgroundColor: '#202124',
+            fontFamily: 'Poppins',
+            padding: '0.5rem 1rem',
+            marginBottom: '2rem',
+            border: '1px solid white',
+            borderRadius: '4px',
+            color: 'white',
+          }}
+          type="password"
+          placeholder="Password"
+        ></input>
+        <button className={'primaryBtn'} type="submit" onClick={handleSubmit}>
+          Submit
+        </button>
+      </div>
+    </form>
+  );
 }
 
 export default Login;
